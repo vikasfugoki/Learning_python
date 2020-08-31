8704d0f96f2adba744842328b975a73e5c093de0
vikasfugoki <vikas2pain@gmail.com>
2020-08-31 17:49:13 +0530

    slight modification using the enumerate function

--- python3.py
+++ python3.py
@@ -1,21 +1,28 @@
 # program to learn about List by making the game Tic Tac Toe
-ga = (0,0,0,
-	0,0,0,
-	0,0,0)
+#ga = (0,0,0,
+#	0,0,0,
+#	0,0,0)
 
 #print (ga)
 #print (type(ga))
 
-gam = [0,0,0,
-0,0,0,
-0,0,0]
+#gam = [0,0,0,
+#0,0,0,
+#0,0,0]
 #print (gam)
 
 game = [[0,0,0],
 [0,0,0],
 [0,0,0]]
 #print (game)
-
-for row in game:
-	print(row)
-
+print ("     0  1  2")
+count = 0
+#for row in game:
+#	print(count, row)
+#	count += 1
+"""
+enumerate function all it does is return the item and its index
+value as we iterate 
+"""
+for count, row in enumerate(game):
+	print(count, row)
