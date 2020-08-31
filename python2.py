5b8e53c2c6e0a9715a9a3b975e82aeb7c615598f
vikasfugoki <vikas2pain@gmail.com>
2020-08-31 14:29:27 +0530

    string tuple for Loop

---
+++ python2.py
@@ -0,0 +1,9 @@
+# in this we implement string
+# tuples-- they are similar to List but they are immutable compared to List which are mutable
+# tuple is light weighted compared to the List
+
+programming_language = "c++", "java", "python", "javascript"
+print (type(programming_language))  # this will print the type i.e tuple for programming_language
+
+for language in programming_language:
+	print(language)
