bfa86e77c99114b267b03a591f1335deab713cbe
vikasfugoki <vikas2pain@gmail.com>
2020-08-31 14:57:29 +0530

    Lists and tic tac tie

---
+++ python3.py
@@ -0,0 +1,21 @@
+# program to learn about List by making the game Tic Tac Toe
+ga = (0,0,0,
+	0,0,0,
+	0,0,0)
+
+#print (ga)
+#print (type(ga))
+
+gam = [0,0,0,
+0,0,0,
+0,0,0]
+#print (gam)
+
+game = [[0,0,0],
+[0,0,0],
+[0,0,0]]
+#print (game)
+
+for row in game:
+	print(row)
+
